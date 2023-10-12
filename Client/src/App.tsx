aababa7cdfdb3e8380afd4553324663416be602b
Moon-794 <moon0795@outlook.com>
2023-10-12 17:55:34 +0100

    Feedback for current mode

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -27,23 +27,41 @@ function App()
         }
     }
 
+    const loginButton = <input id='submit-button' onClick={handleFormButtons} className='form-btn' type="submit" value="Login" />;
+    const registerButton = <input id='register-button' onClick={handleFormButtons} className='form-btn' type="submit" value="Register" />;
 
+    const buttonArray = [];
+    var title: string = appState;
+
+    if(appState == "Login")
+    {
+        buttonArray[0] = loginButton;
+        buttonArray[1] = registerButton;
+    }
+    else
+    {
+        buttonArray[1] = loginButton;
+        buttonArray[0] = registerButton;
+    }
 
     return (
+    <>
+        <div className='header'>    
+            <h1>{title}</h1>
+        </div>
         <div id="main-container" className='flex center'>
-
             <form action="login" method="post" className='flex cols'>
                 <p>Email</p>
                 <input className='txt-input' type="email" name="user-email" id="user-email" />
                 <p>Password</p>
                 <input className='txt-input' type="password" name="user-password" id="user-password" />
                 <ConfirmPassword registering={appState}/>
-                <div className='flex center'>                
-                    <input id='submit-button' onClick={handleFormButtons} className='form-btn' type="submit" value="Login" />
-                    <input id='register-button' onClick={handleFormButtons} className='form-btn' type="submit" value="Register" />
+                <div className='flex center' id="button-div">                
+                    {buttonArray}
                 </div>
             </form>
         </div>
+    </>
     )
 }
 
--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -4,9 +4,6 @@ import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div className='header'>    
-      <h1>LOGIN</h1>
-    </div>
     <App></App>
   </React.StrictMode>,
 )
