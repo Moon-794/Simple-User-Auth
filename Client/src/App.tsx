8b449582293e01c2b41f3ff60095fee657bdc26f
Moon-794 <moon0795@outlook.com>
2023-10-13 18:00:19 +0100

    Redesign

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,63 +1,28 @@
 import './App.css'
 import './ConfirmPW.tsx'
 import React, {useState} from 'react'
-import ConfirmPassword from './ConfirmPW.tsx';
 
 function App()
 {
     const [appState, setAppState] = useState("Login");
 
-    const handleFormButtons = (e: React.FormEvent) =>
-    {
-        e.preventDefault();
-        
-        if(e.currentTarget.id == "register-button")
-        {
-            if(appState != "Register")
-                setAppState("Register");
-            else
-                console.log("Make Post Request for register");
-        }
-        else
-        {
-            if(appState != "Login")
-                setAppState("Login");
-            else
-                console.log("Make Post Request for login");
-        }
-    }
-
-    const loginButton = <input id='submit-button' onClick={handleFormButtons} className='form-btn' type="submit" value="Login" />;
-    const registerButton = <input id='register-button' onClick={handleFormButtons} className='form-btn' type="submit" value="Register" />;
-
-    const buttonArray = [];
-    var title: string = appState;
-
-    if(appState == "Login")
-    {
-        buttonArray[0] = loginButton;
-        buttonArray[1] = registerButton;
-    }
-    else
-    {
-        buttonArray[1] = loginButton;
-        buttonArray[0] = registerButton;
-    }
+    var title: string = appState.toUpperCase();
 
     return (
     <>
-        <div className='header'>    
-            <h1>{title}</h1>
-        </div>
+        
         <div id="main-container" className='flex center'>
+            <div className='header'>    
+                <h1>{title}</h1>
+            </div>
             <form action="login" method="post" className='flex cols'>
                 <p>Email</p>
                 <input className='txt-input' type="email" name="user-email" id="user-email" />
                 <p>Password</p>
                 <input className='txt-input' type="password" name="user-password" id="user-password" />
-                <ConfirmPassword registering={appState}/>
+                <p id='signup'>Sign up</p>
                 <div className='flex center' id="button-div">                
-                    {buttonArray}
+                    <input id='submit-button' className='form-btn' type="submit" value="Login" />;
                 </div>
             </form>
         </div>
